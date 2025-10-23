ec6358e6d49eb4aae97bf2ed3cae8f89b24ab2a9
THE-ASHUTOSH <ashubham1906@gmail.com>
2025-10-24 02:23:36 +0530

    production ready

--- server/index.js
+++ server/index.js
@@ -12,12 +12,27 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
-app.use(cors({
-    origin: FRONTEND_URL,
+const allowedOrigins = [
+  "http://localhost:5173",
+    FRONTEND_URL
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.log("❌ Blocked by CORS:", origin);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
