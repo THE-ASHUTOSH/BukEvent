b77846b403e0b46b4fa8faeaf74b73f8a15c47e6
THE-ASHUTOSH <ashubham1906@gmail.com>
2025-10-24 02:08:44 +0530

    frontendurl changed

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 app.use(cors({
     origin: FRONTEND_URL,
